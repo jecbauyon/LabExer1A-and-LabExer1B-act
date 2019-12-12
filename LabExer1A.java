a78f1ff306810ff664ce0891e4a06dc1e2d3e462
jecbauyon <58801205+jecbauyon@users.noreply.github.com>
2019-12-12 14:01:13 +0800

    Add files via upload

---
+++ LabExer1A.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class LabExer1A{
+	public static void main (String[] args) {
+		int faveNumber =  23;
+		String faveCartChar = "Doraemon";
+		char mi = 'Q';
+		char[] nickNameArray = {'J','e','c'};
+		
+		System.out.println(faveNumber + " is my favorite number.");
+		System.out.println("I love "+ faveCartChar +"!");
+		System.out.println("My Name is Jec " + mi + " Bauyon");
+		System.out.print("You can call me ");				
+		System.out.print(nickNameArray[0]);
+		System.out.print(nickNameArray[1]);
+		System.out.print(nickNameArray[2]);
+		
+}
+}
